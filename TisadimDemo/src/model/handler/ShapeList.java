62d2ffd160f687490496a74219181c738574b0ed
권현정 <@172.30.20.1>
2016-05-29 02:03:40 +0900

    circle

--- TisadimDemo/src/model/handler/ShapeList.java
+++ TisadimDemo/src/model/handler/ShapeList.java
@@ -40,6 +40,37 @@ public class ShapeList implements Serializable{
 	public int getSelectIndex(Point point) {
 		int index = -1;
 		
+		for(int i=shapeList.size()-1;i>=0;i--)
+		{
+			switch(shapeList.get(i).getId())
+			{
+			case 1: //circle
+			{
+				double a = 0, b = 0, ellipse = 0; //a : 장축, b : 단축
+				
+				a = (double)(shapeList.get(i).getEndPoint().x-shapeList.get(i).getStartPoint().x)/(2.0);
+				b = (double)(shapeList.get(i).getEndPoint().y-shapeList.get(i).getStartPoint().y)/(2.0);
+				ellipse = (double)((point.x-shapeList.get(i).getCenterPoint().x)*(point.x-shapeList.get(i).getCenterPoint().x))/(a*a)
+						+(double)((point.y-shapeList.get(i).getCenterPoint().y)*(point.y-shapeList.get(i).getCenterPoint().y))/(b*b);
+				System.out.println(ellipse);
+				if(ellipse<=1)
+				{
+					return i;
+				}
+				break;
+			}
+			case 2: //
+			{
+				
+			}
+			case 3:
+			case 4:
+			case 5:
+			}
+			
+		}
+	
+		
 		return index;
 	}
 }
