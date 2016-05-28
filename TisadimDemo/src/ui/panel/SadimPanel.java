b55769183fbeafc13226284c1936cd9870fd78ac
권현정 <@172.30.20.1>
2016-05-29 00:01:31 +0900

    c

--- TisadimDemo/src/ui/panel/SadimPanel.java
+++ TisadimDemo/src/ui/panel/SadimPanel.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.font.ShapeGraphicAttribute;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 import javax.swing.border.Border;
@@ -44,6 +45,8 @@ public class SadimPanel extends JPanel implements ActionListener, MouseInputList
 	private Curve curve;
 	private Polygon polygon;
 	private Shape shape;
+	
+	private boolean Selectshape;
 
 	ShapeList shapeList;
 	
@@ -61,6 +64,7 @@ public class SadimPanel extends JPanel implements ActionListener, MouseInputList
 		fillColor = Color.black;
 		border = 1;
 		isMakeShape = false;
+		Selectshape = false;
 		
 		canvas = new MyCanvas();
 		canvas.setBounds(0, 0, 800, 600);
@@ -98,60 +102,119 @@ public class SadimPanel extends JPanel implements ActionListener, MouseInputList
 		public void paint(Graphics g) {			
 			super.paint(g);
 			Graphics2D g2 = (Graphics2D)canvas.getGraphics();
-			
-			// 이전까지 그렸던 도형 그림
+
+				// 이전까지 그렸던 도형 그림
 			for(int i=0; i<shapeList.size(); i++) {
-				Shape s = shapeList.get(i);
 				
+				Shape s = shapeList.get(i);
+
 				g2.setColor(s.getEdgeColor());
 				g2.setStroke(new BasicStroke(s.getStroke()));
 				s.drawEdge(g2);
 				
 				g2.setColor(s.getFillColor());
 				s.drawFill(g2);
+
+				if(Selectshape && s.getIsSelect())
+					s.drawSelect(g2);
 			}
 		}
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void mouseClicked(MouseEvent e) 
+	{
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
 		border = Float.valueOf(mainFrame.getSpinner().getValue().toString()).floatValue();
+		
 		if( shapeId == Shape.CIRCLE) {
 			isMakeShape = true;
 			circle = new Circle(edgeColor, fillColor, border);
 			shapeList.addShape(circle);
+			
+			shape = shapeList.get(shapeList.size()-1);
+			shape.doClick(e.getPoint());
+			
+			canvas.repaint();
 		}
 		else if(shapeId == Shape.CURVE) {
 			isMakeShape = true;
 			curve = new Curve(edgeColor, fillColor, border);
 			shapeList.addShape(curve);
+
+			shape = shapeList.get(shapeList.size()-1);
+			shape.doClick(e.getPoint());
+			
+			canvas.repaint();
 		}
 		else if(shapeId == Shape.LINE) {
 			isMakeShape = true;
 			line = new Line(edgeColor, fillColor, border);
 			shapeList.addShape(line);
+			
+			shape = shapeList.get(shapeList.size()-1);
+			shape.doClick(e.getPoint());
+			
+			canvas.repaint();
 		}
 		else if(shapeId == Shape.POLYGON && isMakeShape == false) {
 			isMakeShape = true;
 			polygon = new Polygon(edgeColor, fillColor, border);
 			shapeList.addShape(polygon);
+			
+			shape = shapeList.get(shapeList.size()-1);
+			shape.doClick(e.getPoint());
+			
+			canvas.repaint();
 		}
 		else if(shapeId == Shape.RECTANGLE) {
 			isMakeShape = true;
 			rectangle = new Rectangle(edgeColor, fillColor, border);
 			shapeList.addShape(rectangle);
+			
+			shape = shapeList.get(shapeList.size()-1);
+			shape.doClick(e.getPoint());
+			
+			canvas.repaint();
 		}
-		shape = shapeList.get(shapeList.size()-1);
-		shape.doClick(e.getPoint());
-		
-		canvas.repaint();
+		else if(shapeId == 0)
+		{
+			if(shapeList.size() != 0)
+			{
+				for(int i=shapeList.size()-1;i>=0;i--)
+				{
+					switch(shapeList.get(i).getId())
+					{
+					case 1: //circle
+					{
+						int a = 0, b = 0; //a : 장축, b : 단축
+						
+						a = (shapeList.get(i).getEndPoint().x-shapeList.get(i).getStartPoint().x)/2;
+						b = (shapeList.get(i).getEndPoint().y-shapeList.get(i).getStartPoint().y)/2;
+						System.out.println(shapeList.get(i).getCenterPoint().x);
+						if(((e.getX()-shapeList.get(i).getCenterPoint().x)*(e.getX()-shapeList.get(i).getCenterPoint().x))/(a*a)
+						+((e.getY()-shapeList.get(i).getCenterPoint().y)*(e.getY()-shapeList.get(i).getCenterPoint().y))/(b*b)<=1)
+						{
+							System.out.println("ERER");
+							Selectshape = true;
+							shapeList.get(i).setSelect(true);
+							break;
+						}
+					}
+					case 2:
+					case 3:
+					case 4:
+					case 5:
+					}
+					
+				}
+			}
+		}
 	}
 
 	@Override
@@ -180,9 +243,11 @@ public class SadimPanel extends JPanel implements ActionListener, MouseInputList
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		shape.doPress(e.getPoint());
-		canvas.repaint();
+		if(shapeId != 0)
+		{
+			shape.doPress(e.getPoint());
+			canvas.repaint();
+		}
 	}
 
 	@Override
